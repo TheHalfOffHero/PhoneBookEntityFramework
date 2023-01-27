be0117d2ee1ead66b19781ee001c787f32e68685
Matt Ruiz <mfruiz33@gmail.com>
2023-01-27 01:47:57 -0500

    Add project files.

---
+++ PhoneBook/Data/PhoneBookContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Data
+{
+    internal class PhoneBookContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; } = null!;
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Data Source=LENOVOPRIME;Database=PhoneBookDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+    }
+}
+
---
+++ PhoneBook/Program.cs
@@ -0,0 +1,25 @@
+﻿using PhoneBook.Data;
+using PhoneBook.Models;
+
+namespace PhoneBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new Context object in order to interact with the database
+            using PhoneBookContext context = new PhoneBookContext();
+
+            //Creating a new contact Object
+            Contact Matt = new Contact()
+            {
+                Name = "Matt",
+                PhoneNumber = "123-456-7890"
+            };
+
+            context.Contacts.Add(Matt);
+
+            context.SaveChanges();
+        }
+    }
+}
